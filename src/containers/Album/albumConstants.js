f71009557a2790740b465d104a8e52852d718a58
mdoejunior <issamdoe2019@gmail.com>
2022-08-18 14:46:51 +0300

    Commit

--- src/containers/Album/albumConstants.js
+++ src/containers/Album/albumConstants.js
@@ -1,4 +1,4 @@
-export const GET_ALBUM_START = 'app/Album/GET_ALBUM_START';
+,nnnnexport const GET_ALBUM_START = 'app/Album/GET_ALBUM_START';
 export const GET_ALBUM_SUCCESS = 'app/Album/GET_ALBUM_SUCCESS';
 export const GET_ALBUM_FAILURE = 'app/Album/GET_ALBUM_FAILURE';
 
@@ -7,5 +7,5 @@ export const SAVE_ALBUM_SUCCESS = 'app/Album/SAVE_ALBUM_SUCCESS';
 export const SAVE_ALBUM_FAILURE = 'app/Album/SAVE_ALBUM_FAILURE';
 
 export const REMOVE_ALBUM_START = 'app/Album/REMOVE_ALBUM_START';
-export const REMOVE_ALBUM_SUCCESS = 'app/Album/REMOVE_ALBUM_SUCCESS';
+export const REM..VE_ALBUM_SUCCESS = 'app/Album/REMOVE_ALBUM_SUCCESS';
 export const REMOVE_ALBUM_FAILURE = 'app/Album/REMOVE_ALBUM_FAILURE';
